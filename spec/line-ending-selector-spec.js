2c19b9430e428485023224be9b48a7f66c0ea66e
Jessica Lord <jlord@github.com>
2015-07-27 17:43:48 -0700

    Add line ending selector modal
    Signed-off-by: Max Brunsfeld <maxbrunsfeld@gmail.com>

--- spec/line-ending-selector-spec.js
+++ spec/line-ending-selector-spec.js
@@ -1,7 +1,7 @@
 'use babel';
 
 describe("line ending selector", () => {
-  let lineEndingTile;
+  let lineEndingTile, lineEndingModal, lineEndingSelector;
 
   beforeEach(() => {
     waitsForPromise(() => {
@@ -16,6 +16,10 @@ describe("line ending selector", () => {
 
     runs(() => {
       let statusBar = atom.workspace.getBottomPanels()[0].getItem();
+      lineEndingModal = atom.workspace.getModalPanels()[0];
+      lineEndingSelector = lineEndingModal.getItem();
+      jasmine.attachToDOM(lineEndingSelector[0]);
+
       lineEndingTile = statusBar.getRightTiles()[0].getItem();
       expect(lineEndingTile.className).toBe("line-ending-tile");
       expect(lineEndingTile.textContent).toBe("");
@@ -58,6 +62,41 @@ describe("line ending selector", () => {
         return atom.workspace.open('unix-endings.md').then(() => {
           expect(lineEndingTile.textContent).toBe("LF");
         });
+      });
+    });
+  });
+
+  describe("clicking the tile", () => {
+    beforeEach(() => {
+      waitsForPromise(() => {
+        return atom.workspace.open('unix-endings.md');
+      });
+
+      runs(() => {
+        lineEndingTile.dispatchEvent(new MouseEvent('click', {}));
+      });
+    });
+
+    it("opens the line ending selector modal", () => {
+      expect(lineEndingModal.isVisible()).toBe(true);
+      expect(lineEndingSelector).toHaveFocus();
+      let listItems = lineEndingSelector.list.find('li');
+      expect(listItems[0].textContent).toBe('LF');
+      expect(listItems[1].textContent).toBe('CRLF');
+
+      lineEndingSelector.filterEditorView.getModel().setText('CR');
+
+      advanceClock(100);
+
+      atom.commands.dispatch(lineEndingSelector[0], 'core:confirm');
+      expect(lineEndingModal.isVisible()).toBe(false);
+      expect(lineEndingTile.textContent).toBe('CRLF');
+    });
+
+    describe("when modal is exited", () => {
+      it("leaves the tile selection as-is", () => {
+        atom.commands.dispatch(lineEndingSelector[0], 'core:cancel');
+        expect(lineEndingTile.textContent).toBe('LF');
       });
     });
   });
